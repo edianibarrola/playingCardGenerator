461763dc3a666543c690765166f3be223fc4358b
edianibarrola <edianibarrola@gmail.com>
2020-12-08 18:39:28 +0000

    update

--- src/main.js
+++ src/main.js
@@ -6,8 +6,26 @@ import "./style.css";
 // import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let suits = ["&#9829;", "&#9827;", "&#9830;", "&#9824;"];
   console.log("Edian's Crazy Card Creator");
+  let playingCard = document.querySelector(".playingCard");
+  let topIcon = document.querySelector(".topIcon");
+  let bottomIcon = document.querySelector(".bottomIcon");
+  let middleNumber = document.querySelector(".middleNumber");
+  let randomSuit = suits[Math.floor(Math.random() * 4)];
+  let randomCard = cardType[Math.floor(Math.random() * 12)];
+  topIcon.innerHTML = randomSuit;
+  bottomIcon.innerHTML = randomSuit;
+  middleNumber.innerHTML = randomCard;
+
+  if (randomSuit === "&#9829;" || randomSuit === "&#9830;") {
+    playingCard.classList.add("red");
+    playingCard.classList.remove("black");
+  }
+  if (randomSuit === "&#9824;" || randomSuit === "&#9827;") {
+    playingCard.classList.add("black");
+    playingCard.classList.remove("red");
+  }
 };
 
-//let suits = [hearts, clubs, diamonds, spades];
-//let cardType = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+let cardType = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
