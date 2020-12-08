1df03277d585de54d34dd33cb9853c6b24571548
edianibarrola <edianibarrola@gmail.com>
2020-12-08 17:43:45 +0000

    update

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  console.log("Edian's Crazy Card Creator");
 };
+
+//let suits = [hearts, clubs, diamonds, spades];
+//let cardType = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
